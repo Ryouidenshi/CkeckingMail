187d72b5d511772cfde4b10ca0b72cb63a058ef1
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2021-02-05 22:20:04 +0500

    Add files via upload

---
+++ ConsoleApp40/Mail.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40
+{
+    enum Parts
+    {
+        Login,
+        Service,
+        Domen
+    }
+
+    class Mail
+    {
+        public string Input { get; set; }
+
+        public string Login { get; set; }
+
+        public string Service { get; set; }
+
+        public string Domen { get; set; }
+
+        public bool IsCorrect { get; set; }
+
+        public Mail(string input)
+        {
+            IsCorrect = true;
+            Input = input;
+            CheckForCorrect(0, Parts.Login);
+        }
+
+        private void CheckForCorrect(int index, Parts part)
+        {
+            var sb = new StringBuilder();
+            switch (part)
+            {
+                case Parts.Login:
+                    while (Input[index] != '@' && index < Input.Length - 1)
+                    {
+                        sb.Append(Input[index]);
+                        index++;
+                    }
+                    if (index == Input.Length)
+                    {
+                        IsCorrect = false;
+                    }
+                    sb.Append('@');
+                    Login = sb.ToString();
+                    sb.Clear();
+                    CheckForCorrect(index + 1, Parts.Service);
+                    break;
+                case Parts.Service:
+                    if(index>=Input.Length)
+                    {
+                        IsCorrect = false;
+                        index -= 2;
+                    }
+                    while(Input[index]!='.' && index < Input.Length - 1)
+                    {
+                        sb.Append(Input[index]);
+                        index++;
+                    }
+                    sb.Append('.');
+                    Service = sb.ToString();
+                    sb.Clear();
+                    CheckForCorrect(index + 1, Parts.Domen);
+                    break;
+                case Parts.Domen:
+                    int count = 0;
+                    for (int i = index; i < Input.Length; i++)
+                    {
+                        if (Input[i] == '.')
+                            IsCorrect = false;
+                        sb.Append(Input[i]);
+                        count++;
+                    }
+                    if (count < 2)
+                        IsCorrect = false;
+                    Domen = sb.ToString();
+                    sb.Clear();
+                    break;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var email = new Mail("djonich@mail.ru");
+            Console.Write(email.Input);
+            if (email.IsCorrect)
+                Console.WriteLine(" - Верная запись!");
+        }
+    }
+}
